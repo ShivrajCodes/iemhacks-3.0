9f82d51349f05a364df99eb50e1d3fcc9ead31a8
Shivraj Bhattacharya <shivraj.xe.45@gmail.com>
2025-01-19 20:46:43 +0530

    Initial commit

---
+++ aegiscore/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#080B10",
+        primary: "#1A1F71",
+      },
+    },
+  },
+  plugins: [],
+};
